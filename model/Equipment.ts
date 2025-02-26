dd0301b086d16d7abd12ded201a1e82a1751db54
HimalN <himalnimsara88@gmail.com>
2025-02-27 00:49:06 +0530

    equipments

---
+++ model/Equipment.ts
@@ -0,0 +1,12 @@
+export default class Equipment{
+    eq_code!:string
+    field_location!:string
+    field_name!:string
+    name!:string
+    phone_no!:string
+    role!:string
+    status!:string
+    type!:string
+    field_code!:string
+    staff_id!:string
+}
