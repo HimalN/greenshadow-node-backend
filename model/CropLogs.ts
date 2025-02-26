663703801fba836ff88dcb79a66566a4c6db4e84
HimalN <himalnimsara88@gmail.com>
2025-02-27 00:54:06 +0530

    crop logs

---
+++ model/CropLogs.ts
@@ -0,0 +1,9 @@
+export default class CropLogs{
+    crop_code!:string
+    category!:string
+    common_name!:string
+    img!:string
+    scientific_name!:string
+    season!:string
+    field_code!:string
+}
